903f0c3f3b47086ab6390fba467b810175f7da97
Mauricio Murga <murga.mauricio@gmail.com>
2018-02-20 11:17:02 -0600

    fix folio specs

--- spec/arbol_financiero/folio_spec.rb
+++ spec/arbol_financiero/folio_spec.rb
@@ -1,6 +1,7 @@
 RSpec.describe ArbolFinanciero::Folio do
-  let(:complete_folio_response) { JSON.parse(file_fixture('folio8124.json').read) }
-  let(:basic_folio_response) { JSON.parse(file_fixture('folio278.json').read) }
+  let(:complete_folio_response) { JSON.parse(file_fixture('complete_folio_response.json').read) }
+  let(:basic_folio_response) { JSON.parse(file_fixture('basic_folio_response.json').read) }
+  let(:credit_report_folio_response) { JSON.parse(file_fixture('credit_report_folio_response.json').read) }
   let(:configuration) { YAML.load_file(file_fixture('configuration.yml')) rescue {} }
 
   before do
@@ -11,27 +12,40 @@ RSpec.describe ArbolFinanciero::Folio do
     end
   end
 
+  it "gets a complete folio" do
+    folio_id = '282'
+    allow_any_instance_of(ArbolFinanciero::Requestor).to receive(:request).and_return(complete_folio_response)
+    folio = ArbolFinanciero::Folio.find(folio_id, include: "**")
+
+    expect(folio).to be_a(ArbolFinanciero::Folio)
+    expect(folio.id).to eq(folio_id)
+    expect(folio.verticalType).to eq("personal")
+    expect(folio.included).to be_a(Array)
+    expect(folio.included.select{|i| i.type == "creditReports"}.count).to eq(1)
+    expect(folio.included.select{|i| i.type == "creditReports"}.count).to eq(1)
+  end
+
   it "gets a basic folio" do
-    folio_id = '278'
+    folio_id = '282'
     allow_any_instance_of(ArbolFinanciero::Requestor).to receive(:request).and_return(basic_folio_response)
     folio = ArbolFinanciero::Folio.find(folio_id)
 
     expect(folio).to be_a(ArbolFinanciero::Folio)
     expect(folio.id).to eq(folio_id)
-    expect(folio.verticalType).to eq("education")
+    expect(folio.verticalType).to eq("personal")
     expect(folio.included).to be_a(Array)
+    expect(folio.included.select{|i| i.type == "creditReports"}.count).to eq(0)
   end
 
-  it "gets a complete folio" do
-    folio_id = '8124'
-    allow_any_instance_of(ArbolFinanciero::Requestor).to receive(:request).and_return(complete_folio_response)
-    folio = ArbolFinanciero::Folio.find(folio_id, include: "creditReports")
+  it "gets a folio with creditReports" do
+    folio_id = '282'
+    allow_any_instance_of(ArbolFinanciero::Requestor).to receive(:request).and_return(credit_report_folio_response)
+    folio = ArbolFinanciero::Folio.find(folio_id, include: "applicant.creditReport")
 
     expect(folio).to be_a(ArbolFinanciero::Folio)
     expect(folio.id).to eq(folio_id)
-    expect(folio.verticalType).to eq("education")
+    expect(folio.verticalType).to eq("personal")
     expect(folio.included).to be_a(Array)
-    expect(folio.included.select{|i| i.type == "creditReports"}.count).to eq(2)
-    expect(folio.included.select{|i| i.type == "creditReports"}.first.bureauReportResponse).to be_a(String)
+    expect(folio.included.select{|i| i.type == "creditReports"}.count).to eq(1)
   end
 end
